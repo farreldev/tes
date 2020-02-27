7327d3188d6cd9b924d53b5f3990adb9da8528fa
Say Pulung <saypulung@gmail.com>
2020-02-27 18:23:10 +0700

    add method for calculation

---
+++ src/components/Calculator.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Calculator Biasa</h1>
+</template>
+
+<script>
+export default {
+    name: 'Calculator',
+    methods: {
+        tambah() {
+        },
+        kurang() {
+        },
+        kali() {
+        },
+        bagi() {
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
