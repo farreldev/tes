c45d6e0f0c450587c80f2aa56d248f3c3e4b506f
Say Pulung <saypulung@gmail.com>
2020-02-27 18:29:22 +0700

    buat layout

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Calculator Biasa</h1>
+    <div class="calculator">
+        <div class="content-calculator">
+            <h1>Calculator Biasa</h1>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -18,6 +22,23 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.calculator {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(75, 188, 216);
+    .content-calculator {
+        padding: 20px;
+        border-radius: 5px;
+        min-width: 300px;
+        min-height: 400px;
+        background-color: white;
+        h1 {
+            text-align: center;
+        }
+    }
+}
 </style>
